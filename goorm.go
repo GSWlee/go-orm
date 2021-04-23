e49ffce135f3a632fa2deca940ecc48ad3cca2e3
GSWlee <995030368@qq.com>
2021-04-23 18:25:46 +0800

    完结🎉

--- goorm.go
+++ goorm.go
@@ -5,7 +5,9 @@ import (
 	"./log"
 	"./session"
 	"database/sql"
+	"fmt"
 	_ "github.com/mattn/go-sqlite3"
+	"strings"
 )
 
 type Engine struct {
@@ -64,4 +66,52 @@ func (e *Engine) Transaction(f TxFunc) (result interface{}, err error) {
 		}
 	}()
 	return f(s)
+}
+
+func difference(a []string,b []string)(diff []string){
+	mapB:=make(map[string]bool)
+	for _,v:=range b{
+		mapB[v]=true
+	}
+	for _,v:=range a{
+		if _,ok:=mapB[v];!ok{
+			diff=append(diff,v)
+		}
+	}
+	return
+}
+
+func (e *Engine) Migrate(value interface{}) error {
+	_,err:=e.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable(){
+			log.Info("table %s doesn't exist",s.RefTable().Name)
+			return nil,s.CreateTable()
+		}
+		table:=s.RefTable()
+		rows,_:=s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1",table.Name)).QueryRows()
+		colums,_:=rows.Columns()
+		addCols:=difference(table.FieldName,colums)
+		delCols:=difference(colums,table.FieldName)
+
+		for _,col :=range addCols{
+			f:=table.GetField(col)
+			sqlStr:=fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;",table.Name,f.Name,f.Type)
+			if _,err:=s.Raw(sqlStr).Exec();err!=nil{
+				return nil,err
+			}
+		}
+
+		if len(delCols)==0{
+			return
+		}
+
+		tmp:="tmp_"+table.Name
+		fieldStr :=strings.Join(table.FieldName,", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s FROM %s;",tmp,fieldStr,table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;",table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;",tmp,table.Name))
+		_,err=s.Exec()
+		return
+	})
+	return err
 }
